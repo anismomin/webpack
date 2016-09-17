bd6d019cc62cbe6d84225189228024bc0c520e02
anismomin <anismomin.dev@gmail.com>
2016-09-18 01:36:02 +0500

    basic webpack module bundler working

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,35 @@
-module.exports = {
-    entry: './main.js',
+var path = require('path');
+
+var DIST_DIR = path.resolve(__dirname, 'dist');
+var SRC_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+    entry: SRC_DIR + '/app/main.js',
     output: {
-        filename: './bundle.js'
+        path: DIST_DIR + '/app',
+        filename: 'bundle.js',
+        publicPath: '/app/'
     },
-    watch: true,
-     module: {
+    //watch: true,
+    module: {
         loaders: [
             {
                 test: /\.js$/,
+                include: SRC_DIR,
                 exclude: /node_modules/,
                 loader: 'babel',
                 query: {
                     presets: ['es2015']
                 }
+            },
+            {
+                test: /\.css$/,
+                include: SRC_DIR,
+                exclude: /node_modules/,
+                loader: 'style!css'
             }
         ],
     }
 };
+
+module.exports = config;
